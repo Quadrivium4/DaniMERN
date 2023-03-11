bacaca728d9fc8f6cd58691079295150dcdb30be
Quadrivium <coder.quadrivium4@gmail.com>
2023-03-11 18:13:07 +0100

    cookies hoping fixed
    Former-commit-id: 12bbe71955feae1e757c8f9ae89b0567157b7e60

--- backend/middlewares/verifyUser.js
+++ backend/middlewares/verifyUser.js
@@ -3,7 +3,7 @@ const {User} = require("../models");
 const verifyUser = async (req, res, next) => {
     const email = req.session.userEmail;
     console.log(req.session, )
-    console.log({email: req.session.userEamail})
+    console.log({email: req.session.userEmail})
     if (!email) throw new AppError(NOT_LOGGED_IN, 403, "You should be logged in");
     const user = await User.findOne({ email });
     if (!user) throw new AppError(USER_NOT_FOUND, 404, "User not found");
